035a019bd079eb04691f7c280391dadc06ebad4a
Yagmur <bakiciyagmur@gmail.com>
2019-04-22 10:34:40 +0200

    landing sign-login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login.hbs");
+});
+
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+  User.create({ username, password })
+    .then(logged => {
+      console.log(logged);
+      res.redirect("/home");
+    })
+    .catch(err => next(err));
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup.hbs");
+});
+
+router.post("/login", (req, res, next) => {
+  const { name, surname, newpassword } = req.body;
+  User.create({ name, surname, newpassword })
+    .then(signedup => {
+      console.log(signedup);
+      res.redirect("/home");
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
