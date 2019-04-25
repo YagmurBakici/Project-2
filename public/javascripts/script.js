e64b523417243572ee67cd32761d203f7a7a9ce5
Yagmur <bakiciyagmur@gmail.com>
2019-04-25 14:47:36 +0200

    menu bar dne

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
