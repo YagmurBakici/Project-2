6de4dacafa2810cf276d1950771a0bccf9550686
Yagmur <bakiciyagmur@gmail.com>
2019-04-25 17:54:56 +0200

    tryıng to deploy

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,3 +14,12 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+const init = function() {
+  let items = document.querySelectorAll("section");
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.background = randomColor({ luminosity: "light" });
+  }
+  cssScrollSnapPolyfill();
+};
+init();
